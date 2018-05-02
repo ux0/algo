74aadf51380c7d50bf6b99c9a502c64f3f95bfec
unknown <tqin@23andme.com>
2018-05-01 21:43:51 -0700

    add insert

---
+++ insertion.py
@@ -0,0 +1,15 @@
+import random
+
+def insertion(a):
+ for i in range(1, len(a)):
+  if a[i] < a[i-1]:
+   for j in range(i):
+    if a[i] < a[j]:
+     a[i], a[j] = a[j], a[i]
+
+a = [i for i in range(10)]
+random.shuffle(a)
+
+print(a)
+insertion(a)
+print(a)
